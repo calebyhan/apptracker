c081966105587b798d935ea8e7d4cb98be927c86
Caleb Han <calebhantech@gmail.com>
2023-04-06 09:50:51 -0400

    commit

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ import psutil
 import time
 import win32process
 import tkinter as tk
+from tkinter import ttk
 import datetime
+import json
 
 root = tk.Tk()
 
 
 def update_data(data):
-    with open("data.json", "a") as f:
-        day
+    f = json.load(open("data.json"))
+    today = datetime.date.today().strftime("%d-%m-%Y")
+    if today not in f:
+        f[today] = data
+    else:
+        f[today] = {i: f[today].get(i, 0) + data.get(i, 0) for i in set(f[today]).union(data)}
+    with open("data.json", "w") as file:
+        json.dump(f, file)
 
 
 def check_processes():
@@ -30,8 +38,20 @@ def check_processes():
     update_data(process_time)
 
 
+root.title("Graph Viewer")
 
-# start the infinite loop function
+sidebar_frame = ttk.Frame(root, width=200, padding=10)
+sidebar_frame.pack(side="left", fill="y")
+
+date_label = ttk.Label(sidebar_frame, text="Select a date:")
+date_label.pack()
+date_var = tk.StringVar()
+date_dropdown = ttk.Combobox(sidebar_frame, textvariable=date_var, values=[])
+date_dropdown.pack()
+
+value_label = ttk.Label(root, text="0")
+value_label.pack()
+
 check_processes()
 
 root.mainloop()
